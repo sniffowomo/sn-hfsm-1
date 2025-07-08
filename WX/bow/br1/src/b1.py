f4699719ff19ed8f222095ddac5182dc0a54593f
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-07-08 15:00:38 +0000

    🫦

---
+++ WX/bow/br1/src/b1.py
@@ -0,0 +1,16 @@
+# ///////////////////////////////////
+# 1st test of the module
+# ///////////////////////////////////
+
+# -- imports ---
+
+import os
+from dotenv import load_dotenv
+
+# --- Vars ---
+load_dotenv(src/.azz)
+NOV_T = os.getenv(NOV)
+
+# -- Main Function ----
+
+# ---sub functions ---
