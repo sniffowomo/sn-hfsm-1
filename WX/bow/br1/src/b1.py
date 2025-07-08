38148eed9d083dd5e3c465ecc5a4bddd09fd4fb0
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-07-08 15:07:28 +0000

    🫦

--- WX/bow/br1/src/b1.py
+++ WX/bow/br1/src/b1.py
@@ -4,6 +4,9 @@
 
 # -- imports ---
 
+from browser_use.llm import ChatOpenAI
+from browser_use import Agent
+import asyncio
 import os
 
 from dotenv import load_dotenv
@@ -31,4 +34,16 @@ def brint():
     he1("Brintaz envz")
     rpr(NOV_T)
 
+
 # Exmaple Function 1
+load_dotenv()
+
+
+async def main():
+    agent = Agent(
+        task="Compare the price of gpt-4o and DeepSeek-V3",
+        llm=ChatOpenAI(model="o4-mini", temperature=1.0),
+    )
+    await agent.run()
+
+asyncio.run(main())
